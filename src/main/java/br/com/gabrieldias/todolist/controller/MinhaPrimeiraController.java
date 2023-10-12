53964765ba68aadd4e50dde74b1f40f8cd34a16e
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-12 04:15:41 -0300

    mudanca para teste

--- src/main/java/br/com/gabrieldias/todolist/controller/MinhaPrimeiraController.java
+++ src/main/java/br/com/gabrieldias/todolist/controller/MinhaPrimeiraController.java
@@ -1,12 +1,16 @@
 package br.com.gabrieldias.todolist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/primeiraRota")
 public class MinhaPrimeiraController {
 
+    @GetMapping("/primeiroMetodo")
     public String primeiraMensagem() {
-        
+        return "Funcionou cavalo!";
     }
 }
