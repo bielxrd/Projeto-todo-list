0bf0578b0fe7a07941969344e48fdf7cd849d5d5
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-12 02:09:29 -0300

    Main do nosso projeto usando RestController

---
+++ src/main/java/br/com/gabrieldias/todolist/TodolistApplication.java
@@ -0,0 +1,15 @@
+package br.com.gabrieldias.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TodolistApplication.class, args);
+        
+    }
+
+}
