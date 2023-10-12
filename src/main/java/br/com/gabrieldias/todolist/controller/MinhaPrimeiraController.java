dceef9d26c5e0d7615c0662d3ae954fe2ffac34c
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-12 02:09:01 -0300

    criando a primeira controller usando REST controller

---
+++ src/main/java/br/com/gabrieldias/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,12 @@
+package br.com.gabrieldias.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MinhaPrimeiraController {
+
+    public String primeiraMensagem() {
+        
+    }
+}
