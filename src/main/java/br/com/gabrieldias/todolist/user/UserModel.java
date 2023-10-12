64bed47549006a9c32bbc410e1f4088536803a4e
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-12 04:17:37 -0300

    Modelo do Objeto usuario (primeira implementacao)

---
+++ src/main/java/br/com/gabrieldias/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.gabrieldias.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+
+}
