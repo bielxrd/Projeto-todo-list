60b0b333ca9e58219f0eae16e9b327bdc0652c87
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-12 02:10:41 -0300

    estrutura do sprint initialzr

---
+++ src/test/java/br/com/gabrieldias/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
