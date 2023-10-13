f34a805f1feb6afa856807c9a6fb09fc77aff5f4
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-13 03:33:58 -0300

    uso da funcao do JpaRepository via iUserRepository para criar o usuario

--- src/main/java/br/com/gabrieldias/todolist/user/UserController.java
+++ src/main/java/br/com/gabrieldias/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.gabrieldias.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private iUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
