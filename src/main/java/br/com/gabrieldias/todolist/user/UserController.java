ad914a67749c02bdae32bb6f21c3b6280f1d8443
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-13 03:53:29 -0300

    uso do Autowired para nao deixar userRepository ser null, alteracao do metodo create para ResponseEntity, que retorna uma resposta com base no que for feito, var user procura se existe um username ja cadastrado, se existir, retornar um bad request com msg, se nao existir, salvar os dados e enviar uma mensagem de CREATED

--- src/main/java/br/com/gabrieldias/todolist/user/UserController.java
+++ src/main/java/br/com/gabrieldias/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.gabrieldias.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,13 @@ public class UserController {
     private iUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe");
+        }
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
