b83a0097c3ca15fc07a0c6787793dfcb7f6a6e79
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-13 20:01:53 -0300

    Controller da task para criar a tarefa e salvar no h2 db

---
+++ src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
@@ -0,0 +1,22 @@
+package br.com.gabrieldias.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private iTaskRepository taskRepository;
+
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel) {
+        System.out.println("entrou no controller");
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+}
