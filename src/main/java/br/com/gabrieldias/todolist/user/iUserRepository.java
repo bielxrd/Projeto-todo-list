8d8fdeb4ae9d5096eac81785578aae275137ce32
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-13 03:50:56 -0300

    metodo findByUsername

--- src/main/java/br/com/gabrieldias/todolist/user/iUserRepository.java
+++ src/main/java/br/com/gabrieldias/todolist/user/iUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface iUserRepository extends JpaRepository<UserModel, UUID> {
-
+    UserModel findByUsername(String username);
 
 }
