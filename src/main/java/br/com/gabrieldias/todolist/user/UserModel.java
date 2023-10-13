c76d21b4c49d9668af1331a2bc9e5a94718a7e1c
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-13 03:32:55 -0300

    adicionado notacao Entity para identificar que e uma tabela, adicionado UUID, gerado automaticamente pelo Spring, e creado um LocalDateTime createdAt para guardar qual momento foi criado o usuario

--- src/main/java/br/com/gabrieldias/todolist/user/UserModel.java
+++ src/main/java/br/com/gabrieldias/todolist/user/UserModel.java
@@ -1,12 +1,28 @@
 package br.com.gabrieldias.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
 }
