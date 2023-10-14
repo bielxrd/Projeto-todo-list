1dc29b06efc92e50dc9eaefb3ecec960c5885fb0
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-14 00:01:25 -0300

    metodo list que retorna a lista do usuario

--- src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
@@ -5,12 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -35,5 +33,11 @@ public class TaskController {
         }
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var tasks = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
+        return tasks;
     }
 }
