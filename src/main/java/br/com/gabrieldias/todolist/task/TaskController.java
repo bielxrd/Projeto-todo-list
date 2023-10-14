bacf616d9e47665f65990c04be759bf865b17ecd
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-13 23:22:54 -0300

    ajuste da task controller para salvar o idUser e salvar quando for feita um insert ao tb_tasks

--- src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
@@ -1,10 +1,14 @@
 package br.com.gabrieldias.todolist.task;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +18,10 @@ public class TaskController {
     private iTaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
-        System.out.println("entrou no controller");
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        System.out.println("entrou no controller!");
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }
