124d24894a3eefc25a865b9b39cb79c3bada4884
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-14 00:33:11 -0300

    ajuste para starts with

--- src/main/java/br/com/gabrieldias/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gabrieldias/todolist/filter/FilterTaskAuth.java
@@ -23,7 +23,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         var servletPath = request.getServletPath();
 
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
             //Pegar a autenticacao (usuario e senha)
             var authorization = request.getHeader("Authorization");
 
