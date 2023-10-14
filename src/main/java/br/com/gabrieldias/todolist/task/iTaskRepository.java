e0a514d41fdb2d799f6b19e2f2a716c8b2a7890c
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-14 00:00:35 -0300

    criado funcao da interface que retorna uma listmodel por iduser

--- src/main/java/br/com/gabrieldias/todolist/task/iTaskRepository.java
+++ src/main/java/br/com/gabrieldias/todolist/task/iTaskRepository.java
@@ -2,8 +2,9 @@ package br.com.gabrieldias.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface iTaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByIdUser(UUID idUser);
 }
