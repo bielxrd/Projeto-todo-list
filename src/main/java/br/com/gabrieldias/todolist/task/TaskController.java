34b99007d5761b3d4628022c1bddaa9ee206cbd0
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-14 01:25:33 -0300

    implementacao do metodo CopyNonNullProperties

--- src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
@@ -1,6 +1,6 @@
 package br.com.gabrieldias.todolist.task;
 
-import jakarta.servlet.http.HttpServlet;
+import br.com.gabrieldias.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,9 +43,13 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-            var idUser = request.getAttribute("idUser");
-            taskModel.setIdUser((UUID) idUser);
-            taskModel.setId(id);
-            return this.taskRepository.save(taskModel);
+        var idUser = request.getAttribute("idUser");
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+
+        return this.taskRepository.save(task);
     }
 }
