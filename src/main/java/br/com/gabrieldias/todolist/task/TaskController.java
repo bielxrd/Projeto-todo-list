164a2a3604b37b09fa1bd10232001509ba286925
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-14 00:33:31 -0300

    funcao update de uma tarefa

--- src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrieldias/todolist/task/TaskController.java
@@ -40,4 +40,12 @@ public class TaskController {
         var tasks = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
         return tasks;
     }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+            var idUser = request.getAttribute("idUser");
+            taskModel.setIdUser((UUID) idUser);
+            taskModel.setId(id);
+            return this.taskRepository.save(taskModel);
+    }
 }
