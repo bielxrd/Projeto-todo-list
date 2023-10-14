f6ed7ef2a19b6b122932380e16652c6416f14845
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-14 03:20:02 -0300

    tratamento de erro para tamanho do titulo da tarefa.

--- src/main/java/br/com/gabrieldias/todolist/task/TaskModel.java
+++ src/main/java/br/com/gabrieldias/todolist/task/TaskModel.java
@@ -27,5 +27,10 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-
+    public void setTitulo(String titulo) throws Exception {
+        if(titulo.length() > 50) {
+            throw new Exception("O campo titulo deve conter no maximo 50 caracteres,");
+        }
+        this.titulo = titulo;
+    }
 }
